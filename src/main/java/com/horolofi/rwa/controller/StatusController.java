aae5f49a7bd01b885077d69e5cc4d72af759fe07
abduh-dotcom <muhammadabdu089@gmail.com>
2025-11-26 23:25:11 +0700

    fix: api health check endpoint

--- src/main/java/com/horolofi/rwa/controller/StatusController.java
+++ src/main/java/com/horolofi/rwa/controller/StatusController.java
@@ -1,4 +1,4 @@
-package com.calonkonglo.kelasrutin.be.controller; // Sesuaikan dengan package Anda
+package com.horolofi.rwa.controller; // Sesuaikan dengan package Anda
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
