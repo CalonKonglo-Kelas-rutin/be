2cf9a30c758075622f4438d99f446a7da2339b0f
abduh-dotcom <muhammadabdu089@gmail.com>
2025-11-26 23:53:04 +0700

    add componentscan

--- src/main/java/com/horolofi/rwa/HoroloFiRwaApplication.java
+++ src/main/java/com/horolofi/rwa/HoroloFiRwaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.horolofi.rwa")
 public class HoroloFiRwaApplication  {
 
     public static void main(String[] args) {
