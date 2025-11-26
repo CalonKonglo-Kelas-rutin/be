211d64abc22478241bdc65733b2d46d7dbebad98
abduh-dotcom <muhammadabdu089@gmail.com>
2025-11-26 10:01:15 +0700

    ini project spring boot

---
+++ src/test/java/com/horolofi/rwa/HoroloFiRwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horolofi.rwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoroloFiRwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
