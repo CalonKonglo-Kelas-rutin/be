00a5ebc0711ef0fa0a986f03e0c5ea1db217d06a
abduh-dotcom <muhammadabdu089@gmail.com>
2025-11-29 23:58:02 +0700

    add: API Register User (email+username+address)

--- src/main/java/com/horolofi/rwa/entity/User.java
+++ src/main/java/com/horolofi/rwa/entity/User.java
@@ -17,7 +17,7 @@ public class User {
     @Id
     private String walletAddress;
     
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = false)
     private String username;
     
     @Column(nullable = false, unique = true)
